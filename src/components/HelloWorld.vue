1b74e7570cb69475094a0299654ac8ab4df81c12
Giuseppeworkjujujj <you@example.com>
2023-02-10 21:16:10 +0100

    uno

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,33 @@
 <template>
-  <div>
-  <pippo v-for="(immagine, index) in immagini" :key="index" :immagine="immagine"/>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-6">
+        <rigaimg v-for="(immagine, index) in immagini" :key="index" :immagine="immagine"/>
+      </div>
+      <div class="col-6">
+        <rigaimg1 v-for="(immagine2, index) in immagini2" :key="index" :immagine2="immagine2"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Fumetti from '@/data/Fumetti.js'
-import pippo from '@/components/pippo.vue'
+import rigaimg from '@/components/rigaimg.vue'
+import rigaimg1 from '@/components/rigaimg1.vue'
+
 export default {
   name: 'HelloWorld',
   components: {
-    pippo,
+    rigaimg,
+    rigaimg1,
+
   },
   data(){
     return{
-      immagini:  Fumetti.filmLinks
+      immagini:  Fumetti.filmLinks,
+      immagini2:  Fumetti.filmLinksEcond
+
     }
   }
 }
@@ -22,7 +35,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-div{
+/* div{
   height: 200px;
-}
+} */
 </style>
